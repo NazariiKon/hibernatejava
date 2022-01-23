28162f61e7e6074ab250ece4671e757f839ccb0e
Nazar <nazar.konechniy2@gmail.com>
2022-01-23 16:24:57 +0200

    First commit

---
+++ src/main/java/entities/Role.java
@@ -0,0 +1,19 @@
+package entities;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="tbl_Roles")
+@ToString
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 200, nullable = false)
+    private String name;
+}
